f9188c6dbe91e5b5f9e708ead9651092210eb720
Donk3ylee <boylee.whiting@googlemail.com>
2024-02-24 23:46:50 +0000

    add joke controller

---
+++ app/Http/Controllers/JokeController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Joke;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class JokeController extends Controller
+{
+    // index returns all of the paginated jokes
+    public function index()
+    {
+
+    }
+
+    // create displays the create form
+    public function create()
+    { 
+
+    }
+
+    // store processes the create form, validates it and enters it into the model
+    public function store()
+    {
+
+    }
+
+    // show displays one selected joke from the database
+    public function show()
+    {
+
+    }
+
+    // edit displays the edit form
+    public function edit()
+    {
+
+    }
+
+    // update processes the edit form, validates it and enters the updated entry into the model
+    public function update()
+    {
+
+    }
+    
+    // destroy deletes the selected joke in the model
+    public function destroy()
+    {
+
+    }
+}
