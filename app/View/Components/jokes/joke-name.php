93990af0bb861ec226016f946d9c742c8525869b
Donk3ylee <boylee.whiting@googlemail.com>
2024-02-25 14:38:02 +0000

    changed laravel generated component class name to camel case as it was generating an annoying intellisense error

--- app/View/Components/jokes/joke-name.php
+++ app/View/Components/jokes/joke-name.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class joke-name extends Component
+class jokeName extends Component
 {
     public $name;
     /**
