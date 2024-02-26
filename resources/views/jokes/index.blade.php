18166d19599df0b043b00931c84d2eb948e966ee
Donk3ylee <boylee.whiting@googlemail.com>
2024-02-26 19:50:05 +0000

    add pagination

--- resources/views/jokes/index.blade.php
+++ resources/views/jokes/index.blade.php
@@ -2,9 +2,11 @@
 
         @foreach($jokes as $joke)
 
-            <x-jokes.joke-name :name="$joke->name" class="text-red-500"/>
+            <x-jokes.joke-name :name="$joke->name" class="text-center text-red-500 align-middle from-stone-600"/>
 
         @endforeach
 
-    
+        {{  $jokes->onEachSide(5)->links() }}
+
     </x-layout>
+p
