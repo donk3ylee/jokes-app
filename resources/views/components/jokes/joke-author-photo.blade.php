bc004dc0541a7a2c55e034679feaadc0709426df
Donk3ylee <boylee.whiting@googlemail.com>
2024-03-04 21:15:29 +0000

    add the author signature and photo components

---
+++ resources/views/components/jokes/joke-author-photo.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ $uri }}" height="{{ $size }}" width="{{ $size }}" alt="" {{ $attributes->merge(['class' => 'rounded-full object-cover border-4']) }}>
--- resources/views/jokes/show.blade.php
+++ resources/views/jokes/show.blade.php
@@ -1,13 +1,10 @@
 <x-layout title="{{ $joke->title }}">
-
     <x-jokes.joke-graphic :uri="$joke->graphics" class="max-h-56 w-100" />
     <x-jokes.joke-title :title="$joke->title" class="text-2xl" />
     <x-jokes.joke-text :text="$joke->joke" class="text-base" />
     <x-jokes.joke-punchline :punchline="$joke->punchline" hide=true />
-
-    {{-- {{ $joke->category }}
-    {{ $joke->graphics }}
-    {{ $joke->author_name }}
-    {{ $joke->author_link }}
-    {{ $joke->author_photo }} --}}
+    <div class="inline-flex items-center mt-3">
+        <x-jokes.joke-author-photo :uri="$joke->author_photo" size="50" class="mr-2" />
+        <x-jokes.joke-author-signature :author="$joke->author_name" :link="$joke->author_link" />
+    </div>
 </x-layout>
