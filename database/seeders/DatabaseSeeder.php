1e15991f4916a6f80fafaf74c5c000fcedbbde0d
Donk3ylee <boylee.whiting@googlemail.com>
2024-04-09 19:09:22 +0100

    create a model, controller, migration. factory and seeder for the available categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\AvailableCategory;
 use App\Models\Joke;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -14,6 +16,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
@@ -21,6 +25,34 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+            // This catagories array is used to populate the AvailableCategory model with its initial values.
+            // The available catagories will be added to by the end users over time when a category listed
+            // here isn't available
+        $categories = [
+            'general',
+            'knock knock',
+            'dirty',
+            'black comedy',
+            'yo mama',
+            'bar',
+            'blonde',
+            'cruel',
+            'one liner',
+            'practical',
+            'satirical',
+            'three wishes',
+            'toilet',
+        ];
+
+        foreach ($categories as $category) {
+            AvailableCategory::factory()->create([
+                'category' => $category,
+            ]);
+        }
+
+
         Joke::factory()->count(50)->create();
     }
+
+
 }
